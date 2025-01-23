b7ebfac8951c37d7ad9041f0b2c95c08d8b29106
Brian Johnson <brjo8465@colorado.edu>
2025-01-23 15:43:15 -0600

    Added basic NER, updated HSK dict

--- src/import_HSK.py
+++ src/import_HSK.py
@@ -3,6 +3,7 @@
 ### import packages
 import pandas as pd
 import numpy as np
+import torch
 from sentence_transformers import SentenceTransformer, util
 sentence_model = SentenceTransformer('paraphrase-MiniLM-L6-v2') # model for embedding
 
@@ -10,7 +11,7 @@ sentence_model = SentenceTransformer('paraphrase-MiniLM-L6-v2') # model for embe
 HSK_nums = [('1', 1), ('2', 2), ('3', 3), ('4', 4), ('5', 5), ('6', 6), ('7_9', 7)] # HSK definitions to load files
 HSK = pd.DataFrame()
 for hsk in HSK_nums:
-    hsk_df = pd.read_csv("../data/Chinese/HSK/HSK"+hsk[0]+".tsv", sep="\t", header=None)
+    hsk_df = pd.read_csv("./data/Chinese/HSK/HSK"+hsk[0]+".tsv", sep="\t", header=None)
     hsk_df.drop(0, axis=1, inplace=True)
     hsk_df.rename(columns={1: "character", 2: "pinyin", 3: "definition"}, inplace=True)
     hsk_df.insert(loc=2, column="HSK", value=[hsk[1]]*hsk_df.shape[0])
@@ -20,12 +21,20 @@ HSK.drop_duplicates("character", keep="first", inplace=True)
 HSK.reset_index(inplace=True, drop=True)
 
 ### data containing POS but not definition
-HSK2 = pd.read_csv("../data/Chinese/HSK/hsk30.csv")
+HSK2 = pd.read_csv("./data/Chinese/HSK/hsk30.csv")
 HSK2.rename(columns={'Simplified':'character'}, inplace=True)
 HSK2.drop(labels=['Traditional', 'Pinyin', 'WebNo', 'ID', 'WebPinyin', 'OCR', 'Variants', 'CEDICT','Level'], axis=1, inplace=True)
 
 ### merge
 HSK_full = HSK2.merge(right=HSK, how='right', on='character')
 
+### cross compute the top embeddings for each vocab word and add to HSK dataframe
+similarity_t = sentence_model.similarity(HSK_full["embedding"], HSK_full["embedding"])
+top_choice = torch.flip(np.argsort(similarity_t, axis=1)[:,-21:-1], dims=(1,)).numpy().tolist()
+top_choice_HSK = [[HSK_full["HSK"][i] for i in row] for row in top_choice] # convert from index values to HSK values
+
+HSK_full['top_choice'] = top_choice
+HSK_full['top_choice_level'] = top_choice_HSK
+
 ### save
-HSK_full.to_pickle("../data/Chinese/HSK_full")
+HSK_full.to_pickle("./data/Chinese/HSK_full")
--- src/read_website.py
+++ src/read_website.py
@@ -1,10 +1,81 @@
 """Takes the input url, retreives source html code, replaces words within
 source, generates new html file with replaced words."""
 
+###### import packages, sentence model, and parse html from site url
 import requests
-from readability import Document
+import re
+from bs4 import BeautifulSoup
+import jieba
+import torch
+import string
+import pandas as pd
+import numpy as np
+from sentence_transformers import SentenceTransformer, util
+sentence_model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+language_model = SentenceTransformer('sentence-transformers/distiluse-base-multilingual-cased-v1')
 
-response = requests.get('http://example.com')
-doc = Document(response.content)
-doc.title()
-doc.summary()
+url = 'https://www.bbc.com/zhongwen/articles/c4gl97d2rzjo/simp'
+site = requests.get(url)
+site_soup = BeautifulSoup(site.text, 'html.parser')
+HSK = pd.read_pickle("./data/Chinese/HSK_full")
+# print(site_soup.prettify())
+
+###### get text from the html and tokenize
+site_p = site_soup.find_all('p')
+text = ""
+punc = "！？｡。＂＃＄％＆＇（）＊＋，－／：；＜＝＞＠［＼］＾＿｀｛｜｝～｟｠｢｣､、〃》「」『』【】〔〕〖〗〘〙〚〛〜〝〞〟〰〾〿–—‘’‛“”„‟…‧﹏." # possible punctuation
+
+for p in site_p:
+    text += str(p.get_text()) # append new text to the text string
+
+text.translate(str.maketrans('', '', string.punctuation)) # convert str format
+text_re = re.sub(r"[%s]+" %punc, "", text) # remove punctuation marks
+
+tokens = jieba.lcut(text_re) # tokenize
+tokens_l = a = list(dict.fromkeys(tokens))
+
+###### get text from the html and tokenize
+site_s = site_soup.find_all(string=True)
+text = ""
+punc = "！？｡。＂＃＄％＆＇（）＊＋，－／：；＜＝＞＠［＼］＾＿｀｛｜｝～｟｠｢｣､、〃》「」『』【】〔〕〖〗〘〙〚〛〜〝〞〟〰〾〿–—‘’‛“”„‟…‧﹏." # possible punctuation
+
+for s in site_s:
+    text += str(s.get_text()) # append new text to the text string
+
+text.translate(str.maketrans('', '', string.punctuation)) # convert str format
+text_re = re.sub(r"[%s]+" %punc, "", text) # remove punctuation marks
+
+tokens = jieba.lcut(text_re) # tokenize
+tokens_l = a = list(dict.fromkeys(tokens))
+
+###### Cross compute the top embeddings for each vocab word and add to HSK dataframe
+similarity_t = sentence_model.similarity(HSK["embedding"], HSK["embedding"])
+top_choice = torch.flip(np.argsort(similarity_t, axis=1)[:,-21:-1], dims=(1,)).numpy().tolist()
+top_choice_HSK = [[HSK["HSK"][i] for i in row] for row in top_choice] # convert from index values to HSK values
+
+def simplify(tokens, max_HSK):
+    simplified_tokens = dict()
+    for token in tokens:
+        try:
+            char_idx = np.where(HSK["character"]==token)[0][0]
+            if HSK["HSK"][char_idx]>max_HSK:
+                top_idx = top_choice[char_idx][next(x[0] for x in enumerate(top_choice_HSK[char_idx]) if x[1] <= max_HSK)] # iterate through and find index of first element below max_HSK
+                simplified_tokens[token] = HSK["character"].loc[top_idx]
+            else:
+                pass# simplified_tokens[idx] = 0
+        except:
+            pass# simplified_tokens[idx] = 1
+    return simplified_tokens
+
+replacement_dict = simplify(tokens_l, 4)
+
+for element in site_soup.find_all(string=True):  # Get all text nodes
+    text = element
+    for old_word, new_word in replacement_dict.items():
+        if old_word in text:
+            text = text.replace(old_word, f'<span style="color: red;">{new_word}</span>')
+    element.replace_with(BeautifulSoup(text, 'html.parser'))
+
+with open("output.html", "w", encoding = 'utf-8') as file: 
+    # prettify the soup object and convert it into a string 
+    file.write(str(site_soup.prettify())) 
