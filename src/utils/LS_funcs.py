308a380e729b9198780fd43324ed8a94a9129987
Brian Johnson <brjo8465@colorado.edu>
2025-02-14 16:00:47 -0600

    Updated POS tagging for similarity, bug fixes in LS pipeline

--- src/utils/LS_funcs.py
+++ src/utils/LS_funcs.py
@@ -18,16 +18,17 @@ ner_pipeline = pipeline(Tasks.named_entity_recognition, 'damo/nlp_raner_named-en
 ###### Load vocab data
 with open("../data/BLCU/top_similar.pickle", 'rb') as handle:
     top_similar = pickle.load(handle)
-with open("../data/BLCU/similarity_dict.pickle", 'rb') as handle:
+with open("../data/BLCU/similarity_dict_v2.pickle", 'rb') as handle:
     similarity_dict = pickle.load(handle)
 with open("../data/HSK/HSK_levels.pickle", 'rb') as handle:
     hsk_dict = pickle.load(handle)
 # punc = ",!?！？｡。＂＃＄％＆＇（）＊＋，－／：；＜＝＞＠［＼］＾＿｀｛｜｝～｟｠｢｣､、〃》「」『』【】〔〕〖〗〘〙〚〛〜〝〞〟〰〾〿–—‘’‛“”„‟…‧﹏.-" # possible punctuation
 blcu = pd.read_csv('../data/BLCU/literature_wordfreq.release_UTF-8.txt', header = None, sep="\t",)
 blcu.rename(columns={0:"character", 1:"frequency"}, inplace=True)
+blcu.set_index("character", inplace=True)
 blcu["frequency"] = blcu["frequency"].rank(pct=True)
 
-def LS_pipeline(sentence: str):
+def LS_pipeline(sentence: str, verbose:bool = False):
     """End-to-end pipeline for lexical simplification of a sentence."""
     ### tokenize sentence:
     tokens = jieba.lcut(sentence)
@@ -45,17 +46,30 @@ def LS_pipeline(sentence: str):
     simple_sentence = sentence
     for word in complex_words:
         candidates = similarity_dict[word]
+        # remove candidates with lower frequency:
+        word_freq = blcu.loc[word].values[0]
+        for candidate in candidates:
+            cand_freq = blcu.loc[candidate].values[0]
+            if cand_freq < word_freq*0.99:
+                candidates.remove(candidate)
+
         ### select final candidate, replace in sentence:
         if candidates:
             simple_sentence = choose_and_replace(simple_sentence, word, candidates) # update new sentence with replaced words
 
+    if verbose:
+        print("Original sentence:\n", sentence)
+        print("Simplified sentence:\n", simple_sentence)
+        for word in complex_words:
+            print("Complex word: ", word)
+            print("Candidates: ", [f"{cand}" for cand in similarity_dict[word]])
     return simple_sentence
 
-def find_complex_words(tokens: list, HSK_thresh:int = 5, freq_thresh:float = 0.97):
+def find_complex_words(tokens: list, HSK_thresh:int = 5, freq_thresh:float = 0.98):
     """Finds complex words based on one of two criteria: HSK level, and frequency in BLCU data. Assumes
     less frequent words are more difficult or less likely to be known."""
     complex_HSK = [token for token in tokens if (token in hsk_dict and hsk_dict[token]>HSK_thresh)] # find all tokens above HSK level
-    complex_freq = [token for token in tokens if (token in blcu.index and blcu.loc[tokens[0]].values[0]<freq_thresh)] # find all tokens below freq level
+    complex_freq = [token for token in tokens if (token in blcu.index and blcu.loc[token].values[0]<freq_thresh)] # find all tokens below freq level
     complex_words = list(set(complex_HSK).union(complex_freq)) # combine by union
     return complex_words
 
@@ -66,7 +80,7 @@ def choose_and_replace(sentence: str, word: str, candidates: list):
         new_sentence = sentence.replace(word, candidate) # new sentence with word replaced
         embed_new = model.encode(new_sentence) # create embeddings
         similarity.append(np.float32(model.similarity(embed_orig, embed_new)[0][0])) # calculate similarity
-    
+    #TODO: only finds best based on cos sim, but the possible replacement could have very low frequency
     best_idx = int(np.argmax(similarity))
     if best_idx!=0:
         print("Best index is", best_idx)
--- src/utils/generate_similarity_dict.py
+++ src/utils/generate_similarity_dict.py
@@ -8,6 +8,8 @@ import pickle
 from sentence_transformers import SentenceTransformer
 model = SentenceTransformer('BAAI/bge-large-zh-v1.5')
 
+#TODO: Fix dataframe.iloc statements for building similarity dict
+
 ###### read raw data
 df = pd.read_csv('./data/BLCU/literature_wordfreq.release_UTF-8.txt', header = None, sep="\t",)
 df.rename(columns={0:"character", 1:"frequency"}, inplace=True)
@@ -26,7 +28,7 @@ top_similar = torch.flip(np.argsort(similarity_t, axis=1)[:,-16:-1], dims=(1,)).
 with open("./data/BLCU/top_similar.pickle", 'wb') as handle:
     pickle.dump(top_similar, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-###### convert results to dict with matching POS tag
+###### convert results to dict with matching POS tag V1: Using jieba
 similarity_dict = {}
 for idx in range(len(top_similar)):
     base_word = df.iloc[idx,0]
@@ -37,6 +39,25 @@ for idx in range(len(top_similar)):
     for word in top_list:
         tagged_word = list(pseg.cut(word))[0]  # POS tagging
         if tagged_word.flag==flag: 
+            idx_list.append(word)
+    if idx%500==0:
+        print(idx)
+    similarity_dict[base_word] = idx_list
+
+###### convert results to dict with matching POS tag V2: using thulac
+import thulac   
+thu1 = thulac.thulac()
+
+similarity_dict = {}
+for idx in range(len(top_similar)):
+    base_word = df.iloc[idx,0]
+    top_list = list(df.iloc[top_similar[idx],0])
+    flag = thu1.cut(base_word)[0][1]
+    idx_list = []
+
+    for word in top_list:
+        tagged_word = thu1.cut(word)[0][1]  # POS tagging
+        if tagged_word==flag: 
             idx_list.append(word)
     if idx%500==0:
         print(idx)
