c2942e96082e286612ba311082b5c7291b4ee7ee
Brian Johnson <brjo8465@colorado.edu>
2025-02-24 14:33:44 -0600

    combined commit

--- src/utils/TS_pipeline.py
+++ src/utils/TS_pipeline.py
@@ -9,12 +9,13 @@ def TS_with_BART(sentence: str):
     simple_sentence = text2text_generator(sentence, max_length=128, do_sample=False)[0]['generated_text'].replace(" ","")
     return simple_sentence
 
+def TS_with_LS_BART(sentence: str):
+    ls_sentence = LS.LS_pipeline(sentence)
+    simple_sentence = TS_with_BART(ls_sentence)
+    return simple_sentence
+
 def TS_with_BART_LS(sentence: str):
     bart_sentence = TS_with_BART(sentence)
     simple_sentence = LS.LS_pipeline(bart_sentence)
     return simple_sentence
 
-def TS_with_LS_BART(sentence: str):
-    ls_sentence = LS.LS_pipeline(sentence)
-    simple_sentence = TS_with_BART(ls_sentence)
-    return simple_sentence
