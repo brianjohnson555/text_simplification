36e8e9435c0523f752f9c5eee084b5c3b8b5ab71
Brian Johnson <brjo8465@colorado.edu>
2025-02-27 13:38:50 -0600

    hyperparameter fix with preprocessing logit

--- src/utils/TS_pipeline.py
+++ src/utils/TS_pipeline.py
@@ -1,8 +1,8 @@
 """Pipeline for both text simplification (TS) with BART, and combined approach with LS_BART and BART_LS"""
 import utils.LS_pipeline as LS
 from transformers import BertTokenizer, BartForConditionalGeneration, Text2TextGenerationPipeline
-tokenizer = BertTokenizer.from_pretrained("../data/models/batch_128_15625_500000", local_files_only=True)
-model = BartForConditionalGeneration.from_pretrained("../data/models/batch_128_15625_500000", local_files_only=True)
+tokenizer = BertTokenizer.from_pretrained("../data/models/checkpoint_14064", local_files_only=True)
+model = BartForConditionalGeneration.from_pretrained("../data/models/checkpoint_14064", local_files_only=True)
 text2text_generator = Text2TextGenerationPipeline(model, tokenizer)
 
 def TS_with_BART(sentence: str):
